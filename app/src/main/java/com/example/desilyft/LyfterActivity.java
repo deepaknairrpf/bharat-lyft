7d9f58e99569cbbfe26b73a4d04c051cab0bcce8
dingusagar <dingusagar@gmail.com>
2019-01-18 15:05:52 +0530

    added autocomplete_places API

--- app/src/main/java/com/example/desilyft/LyfterActivity.java
+++ app/src/main/java/com/example/desilyft/LyfterActivity.java
@@ -3,17 +3,25 @@ package com.example.desilyft;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.libizo.CustomEditText;
 
 public class LyfterActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Place destination;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +31,26 @@ public class LyfterActivity extends FragmentActivity implements OnMapReadyCallba
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        /*AutocompleteFilter filter = new AutocompleteFilter.Builder()
+                .setCountry("IN")
+                .setTypeFilter(AutocompleteFilter.TYPE_FILTER_CITIES)
+                .build();
+        autocompleteFragment.setFilter(filter);*/
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                destination = place;
+                Toast.makeText(getApplicationContext(),"Destination Set to "+place.getName(),Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onError(Status status) {
+                Log.d("DEBUG",status.toString());
+            }
+        });
     }
 
 
